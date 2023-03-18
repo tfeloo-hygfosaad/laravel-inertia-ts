263c9ea3a6e320a78ccb84ebab11ba33a638fee2
tfeloo-hygfosaad <tfeloo@yandex.ru>
2023-03-18 11:16:30 +1100

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,30 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es2021": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  env: {
+    browser: true,
+    node: true,
   },
-}
+  rules: {
+    'linebreak-style': 'off',
+    'max-len': ['error', {
+      code: 120, tabWidth: 2, ignoreStrings: true, ignoreTemplateLiterals: true,
+    }],
+    'padding-line-between-statements': ['error', { blankLine: 'always', prev: 'multiline-const', next: '*' }],
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+};
